488597db02a9db859393f98b9fde677a1edcf620
Mikhail Andreev <mikha.andreev@gmail.com>
2014-07-06 22:01:07 -0400

    Integrated file parsing. Using the new getNextSection and findPatternAfterIndex from StrOps.

--- XMLReader/src/xml_parser/XML_Parser.java
+++ XMLReader/src/xml_parser/XML_Parser.java
@@ -3,11 +3,46 @@ package xml_parser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import string_operations.StrOps;
 
 public class XML_Parser {
+	ArrayList<String> opening_sequences = new ArrayList<String>();
+	ArrayList<String> closing_sequences = new ArrayList<String>();
+	
+	public XML_Parser(){
+		opening_sequences.add("<");
+		closing_sequences.add(">");
+		opening_sequences.add("\"");
+		closing_sequences.add("\"");
+		opening_sequences.add("\'");
+		closing_sequences.add("\'");
+		opening_sequences.add(">");
+		closing_sequences.add("<");
+	}
 	
 	public void parseXML(String XMLFile){
 		String file = readFile(XMLFile);
+		int index = 0;
+		
+		String line = StrOps.getNextSection(file, opening_sequences, closing_sequences, index);
+		while(line != null){
+			System.out.println(line);
+			index = updateIndex(file, line, index);
+			line = StrOps.getNextSection(file, opening_sequences, closing_sequences, index);
+		}
+		
+		
+	}
+	
+	private int updateIndex(String file, String line, int index){
+		int new_ind = StrOps.findPatternAfterIndex(file, line, index);
+		if(new_ind == -1){
+			System.out.println("[ERROR] XML_Parser.updateIndex error :: findPatternAfterIndex returned -1");
+			return -1;
+		}
+		return (new_ind + line.length() - 1);
 	}
 	
 	private String readFile(String XMLFile){
